16559b891205688112abed6ecfc0af520b1d5755
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-03-06 15:55:18 +0100

    Lab0

---
+++ Lab0/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz test = new FizzBuzz(20);
+            test.Fun();
+        }
+    }
+    class FizzBuzz
+    {
+        private int cap;
+
+        public FizzBuzz(int capacity)
+        {
+            cap = capacity;
+        }
+        
+        public void Fun()
+        {
+            for(int i = 1; i <= cap;i++)
+            {
+                string output = "";
+
+                if(i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                if(i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                if(output == "")
+                {
+                    output += i.ToString();
+                }
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
