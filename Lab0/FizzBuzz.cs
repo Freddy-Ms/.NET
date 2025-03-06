11fd89a71eb6e20c9c467f462c7fefc684560989
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-03-06 17:38:33 +0100

    lab0

---
+++ Lab0/FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class FizzBuzz
+    {
+        private int cap;
+
+        public FizzBuzz(int capacity)
+        {
+            cap = capacity;
+        }
+
+        public void Fun()
+        {
+            for (int i = 1; i <= cap; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                if (output == "")
+                {
+                    output += i.ToString();
+                }
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
--- Lab0/Program.cs
+++ Lab0/Program.cs
@@ -9,35 +9,5 @@
             test.Fun();
         }
     }
-    class FizzBuzz
-    {
-        private int cap;
-
-        public FizzBuzz(int capacity)
-        {
-            cap = capacity;
-        }
-        
-        public void Fun()
-        {
-            for(int i = 1; i <= cap;i++)
-            {
-                string output = "";
-
-                if(i % 3 == 0)
-                {
-                    output += "Fizz";
-                }
-                if(i % 5 == 0)
-                {
-                    output += "Buzz";
-                }
-                if(output == "")
-                {
-                    output += i.ToString();
-                }
-                Console.WriteLine(output);
-            }
-        }
-    }
+    
 }
