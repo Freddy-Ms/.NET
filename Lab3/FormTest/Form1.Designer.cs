cbe56911a7b1d95f6fb127752ba91832797aa7bd
Freddy-Ms <maciejskrzypa2705@gmail.com>
2025-03-27 00:30:24 +0100

    s

--- Lab3/FormTest/Form1.Designer.cs
+++ Lab3/FormTest/Form1.Designer.cs
@@ -30,42 +30,115 @@
         {
             listBox1 = new ListBox();
             button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 25;
-            listBox1.Location = new Point(203, 24);
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(28, 11);
+            listBox1.Margin = new Padding(2);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(358, 304);
+            listBox1.Size = new Size(252, 184);
             listBox1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(328, 370);
+            button1.Location = new Point(100, 222);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(112, 34);
+            button1.Size = new Size(78, 20);
             button1.TabIndex = 1;
             button1.Text = "Download";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Location = new Point(398, 199);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Convert";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(401, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(401, 121);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(427, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 5;
+            label1.Text = "USD";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(434, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 6;
+            label2.Text = "PLN";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(301, 159);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 7;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(560, 270);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(listBox1);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ListBox listBox1;
         private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+
     }
 }
--- Lab3/FormTest/Form1.cs
+++ Lab3/FormTest/Form1.cs
@@ -1,6 +1,8 @@
+using System.Security.Authentication;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API;
+using static System.Net.WebRequestMethods;
 
 namespace FormTest
 {
@@ -11,6 +13,12 @@ namespace FormTest
         {
             InitializeComponent();
             client = new HttpClient();
+            LoadISAsync();
+        }
+
+        private async void LoadISAsync()
+        {
+            await GlobalConfig.LoadIDAsync();
         }
 
         private async void button1_Click(object sender, EventArgs e)
@@ -23,11 +31,28 @@ namespace FormTest
             {
                 currencies.Add(new Currencies { shortcut = kvp.Key, fullname = kvp.Value });
             }
-       
-            foreach(Currencies curr in currencies)
+
+            foreach (Currencies curr in currencies)
             {
                 listBox1.Items.Add(curr.ToString());
             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           string data = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+           string call = $"https://openexchangerates.org/api/historical/{data}.json?app_id={GlobalConfig.ID}";
+            string response = client.GetStringAsync(call).Result;
+            var info = JsonSerializer.Deserialize<Dictionary<string,JsonElement>>(response);
+            decimal rate = info["rates"].GetProperty("PLN").GetDecimal();
+
+            decimal output = rate * decimal.Parse(textBox1.Text);
+            textBox2.Text = output.ToString();
         }
     }
 }
